7b3e093ed690f19f64c96ea3cedc00f06250969c
zeng715734199 <9558145+zeng715734199@user.noreply.gitee.com>
2026-09-08 14:44:51 +0800

    feat(接口): 导出业务路由模块

--- backend/src/routers/__init__.py
+++ backend/src/routers/__init__.py
@@ -1,1 +1,10 @@
 """接口路由包。"""
+
+from . import competitor_router, content_router, listing_router, supply_router
+
+__all__ = [
+    "competitor_router",
+    "content_router",
+    "listing_router",
+    "supply_router",
+]
